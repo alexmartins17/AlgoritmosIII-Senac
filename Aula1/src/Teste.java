90f80f155e9a911f72b822c37351c0e910c4cea4
Alex Martins <smartins.alex@gmail.com>
2018-08-15 19:54:44 -0300

    Exercício Aula1 finalizado

--- Aula1/src/Teste.java
+++ Aula1/src/Teste.java
@@ -3,18 +3,23 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		/*List l1 = new List();
-		List l2 = new List();
-		List l3 = new List();
+		//Listas criadas para teste
+		List l1 = new List(); // addFirst
+		List l2 = new List(); // insertBefore
+		List l3 = new List(); // insertAfter
 		
-		for(int i = 1; i <= 100; i++){
+		long inicio = System.currentTimeMillis();
+		for(int i = 1; i <= 1000000; i++){
 			l1.addFirst("Alex" + i);
 		}
-
+		long fim = System.currentTimeMillis();
+		System.out.println("Tempo de execução - addFirst, Lista:" + (fim - inicio));
+		
 		l2.addFirst("Alex" + 20000000);
 		l2.addFirst("Alex" + 0);
 		
-		for(int i = 1; i <= 100; i++){
+		inicio = System.currentTimeMillis();
+		for(int i = 1; i <= 100000; i++){
 			Node aux = new Node("");
 			aux = l2.getHead();
 			while(true != false){
@@ -25,13 +30,17 @@ public class Teste {
 				aux = aux.getNext();
 			}
 		}
+		fim = System.currentTimeMillis();
+		System.out.println("Tempo de execução - insertBefore, Lista:" + (fim - inicio));
+		
 		
 		l3.addFirst("João");
 		l3.addFirst("Alex");
 		l3.addFirst("Alex" + 20000000);
 		l3.addFirst("Alex" + 0);
 		
-		for(int i = 1; i <= 100; i++){
+		inicio = System.currentTimeMillis();
+		for(int i = 1; i <= 1000000; i++){
 			Node aux = new Node("");
 			aux = l3.getHead();
 			while(true != false){
@@ -42,31 +51,41 @@ public class Teste {
 				aux = aux.getNext();
 			}
 		}
+		fim = System.currentTimeMillis();
+		System.out.println("Tempo de execução - insertAfter, Lista:" + (fim - inicio));
 		
-		l1.print();
-		l2.print();
-		l3.print();
-		*/
-		Vetor v1 = new Vetor(100);
-		Vetor v2 = new Vetor(100);
-		Vetor v3 = new Vetor(100);
+		//l1.print();
+		//l2.print();
+		//l3.print();
 		
-		for(int i = 0; i < 100; i++){
+		Vetor v1 = new Vetor(100000);
+		Vetor v2 = new Vetor(1000000);
+		Vetor v3 = new Vetor(1000000);
+		
+		inicio = System.currentTimeMillis();
+		for(int i = 0; i < 100000; i++){
 			v1.addFirst("Bruna" + i);
 		}
+		fim = System.currentTimeMillis();
+		System.out.println("Tempo de execução - addFirst, Vetor:" + (fim - inicio));
 		
-		for(int i = 0; i < 100; i++){
+		inicio = System.currentTimeMillis();
+		for(int i = 0; i < 1000000; i++){
 			v2.addLast("Bruno" + i);
 		}
+		fim = System.currentTimeMillis();
+		System.out.println("Tempo de execução - addLast, Vetor:" + (fim - inicio));
 		
-		for(int i = 0; i < 100; i++){
+		inicio = System.currentTimeMillis();
+		for(int i = 0; i < 1000000; i++){
 			v3.insertIndex("Jonas" + i, i);
 		}
+		fim = System.currentTimeMillis();
+		System.out.println("Tempo de execução - insertIndex, Lista:" + (fim - inicio));
 		
-		
-		v1.print();
-		v2.print();
-		v3.print();
+		//v1.print();
+		//v2.print();
+		//v3.print();
 	}
 
 }
